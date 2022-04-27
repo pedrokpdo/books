69f2d6a4ef9f1f3d2c8c0986ffa791d93f065704
pedrokpdo <pedromotoehm@gmail.com>
2022-04-27 13:12:40 -0300

    alugar itens

--- pages/Components/AlugarItem.tsx
+++ pages/Components/AlugarItem.tsx
@@ -1,10 +1,19 @@
 import { Card } from 'antd';
 import Image from 'next/image';
 
-export default function AlugarItem({ nomeLivro, descricaoLivro, imgLivro }: any) {
+export const AlugarItem = ({ nomeLivro, descricaoLivro, imgLivro, setEstado }:any) =>  {
+
+    
+
+
+    function handleState () {
+       let res = confirm('deseja alugar o livro?')
+       if (res === true) {setEstado('alugado')}else ''
+    }
+
 
     return (
-        <>
+        <>  
             <Card title={nomeLivro} extra={<a href="#">More</a>} style={{ width: 800 }}>
                 <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
                     <div style={{ display: 'flex' }}>
@@ -12,8 +21,8 @@ export default function AlugarItem({ nomeLivro, descricaoLivro, imgLivro }: any)
                     </div>
                     <div style={{ width: '400px' }}>
                         <p>{descricaoLivro}</p>
-                        <div style={{display:'flex', justifyContent:'space-around'}}>
-                            <a onClick={() => confirm('deseja alugar o livro?')}>Alugar</a>
+                        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                            <a onClick={handleState}>Alugar</a>
                             <a href="">Comprar</a>
                         </div>
                     </div>
